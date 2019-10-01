e07726bb01e3ec75f6c567a4a593ad0287f7014c
joshuamhtsang <joshua.mh.tsang@gmail.com>
2019-10-01 17:33:03 +0100

    create person table query

--- inject_data_postgres.py
+++ inject_data_postgres.py
@@ -1,6 +1,31 @@
 from psycopg2 import connect
 from faker import Faker
 
+
+def create_person_table_query():
+    query = """
+    CREATE TABLE public.person
+    (
+        id uuid NOT NULL,
+        role text,
+        first_name text,
+        middle_name text,
+        last_name text,
+        nationality text,
+        PRIMARY KEY (id)
+    )
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+    
+    ALTER TABLE public.person
+        OWNER to admin;
+    """
+
+    return query
+
+
 if __name__ == '__main__':
     print("Inject data into PostgreSQL")
 
@@ -11,3 +36,11 @@ if __name__ == '__main__':
         user="admin",
         password="admin"
     )
+    cur = conn.cursor()
+
+    cur.execute(create_person_table_query())
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
